2ceb24725a99257ddf69cd02fe405cf4c158e984
maxvincent24 <maxime.vincent@philiaweb.com>
2022-03-11 19:29:41 +0100

    Fix bug on filename in dtomzML.py

--- msconvert/dtomzML.py
+++ msconvert/dtomzML.py
@@ -12,6 +12,9 @@ print(f'Original data is located in : {path_data}')
 
 path_data_basename = path_data.split('/')[-1]
 path_data_prefix = '/'.join(path_data.split('/')[:-1])
+if path_data_prefix != '':
+	path_data_prefix += '/'
+print(f'path_data_prefix : {path_data_prefix}')
 subprocess.run(f'mkdir {path_data_basename}_mzML'.split(' '))
 
 command = f'find {path_data} -type d -name "*.d"'
@@ -20,7 +23,7 @@ output = subprocess.check_output(command, encoding='utf-8', shell=True)
 list_d_files = output.split('\n')
 list_d_files = [file for file in list_d_files if file != '']
 list_d_files_msconvert = list_d_files
-list_d_files = [file.replace(path_data_prefix + '/', '') for file in list_d_files]
+list_d_files = [file.replace(path_data_prefix, '') for file in list_d_files]
 list_d_files = ['/'.join(file.split('/')[:-1]) for file in list_d_files]
 list_d_files = list(set(list_d_files))
 list_d_files = sorted(list_d_files)
@@ -34,7 +37,7 @@ for directory in list_d_files:
 dir_to_create = sorted(list(set(dir_to_create)))
 dir_to_create = [dir_.replace(path_data_basename, path_data_basename + '_mzML') for dir_ in dir_to_create]
 for elt in dir_to_create:
-	print(elt)
+	print(f'elt : {elt}')
 	subprocess.run(f'mkdir {elt}'.split(' '))
 
 print()
@@ -44,10 +47,10 @@ pwd = pwd.stdout.replace('\n', '')
 
 for file in list_d_files_msconvert:
 
-	print(file)
-	output = '/'.join(file.replace(path_data_prefix, '')[1:].split('/')[:-1]).replace(path_data_basename, path_data_basename + '_mzML')
-	print(output)
+	print(f'file : {file}')
+	output = '/'.join(file.replace(path_data_prefix, '').split('/')[:-1]).replace(path_data_basename, path_data_basename + '_mzML')
+	print(f'output : {output}')
 	print()
 	command = f'singularity run --writable --bind {pwd}:/data img_msconvert/ wine msconvert {file} --filter "peakPicking true 1" -o {output}'
-	print(command)
+	print(f'command : {command}')
 	subprocess.run(command.split(' '))
