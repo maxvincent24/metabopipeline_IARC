4dccdc8f3645b81ccdfa7ae6449db8ddf1b9f86a
maxvincent24 <maxime.vincent@philiaweb.com>
2022-05-16 00:09:52 +0200

    Change docker image version

--- notebooks/run_commands_docker.sh
+++ notebooks/run_commands_docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [[ "$(docker images -q maxvin/data_science_img:0.3.1 2> /dev/null)" == "" ]]; then
-  docker pull maxvin/data_science_img:0.3.1
+if [[ "$(docker images -q maxvin/data_science_img:0.1.5 2> /dev/null)" == "" ]]; then
+  docker pull maxvin/data_science_img:0.1.5
 fi
 
 if [[ ! -d ./metabopipeline_notebooks ]]; then
-    git clone https://github.com/maxvincent24/metabopipeline_notebooks.git
+  git clone https://github.com/maxvincent24/metabopipeline_notebooks.git
 fi
 
-docker run -it -p 8888:8888 -v "${PWD}"/metabopipeline_notebooks:/home/jovyan/work maxvin/data_science_img:0.3.1
+docker run -it -p 8888:8888 -v "${PWD}"/metabopipeline_notebooks:/home/jovyan/work maxvin/data_science_img:0.1.5
