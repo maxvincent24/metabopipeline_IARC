a9dbfafc6d6081ef7230e275f87d5650e85165c6
maxvincent24 <maxime.vincent@philiaweb.com>
2022-11-09 17:37:45 +0100

    Create variable for img version in docker script

--- notebooks/run_commands_docker.sh
+++ notebooks/run_commands_docker.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [[ "$(docker images -q maxvin/data_science_img:0.1.5 2> /dev/null)" == "" ]]; then
-  docker pull maxvin/data_science_img:0.1.5
+img="maxvin/data_science_img:0.1.6"
+
+
+if [[ "$(docker images -q ${img} 2> /dev/null)" == "" ]]; then
+  docker pull ${img}
 fi
 
 if [[ ! -d ./metabopipeline_notebooks ]]; then
   git clone https://github.com/maxvincent24/metabopipeline_notebooks.git
 fi
 
-docker run -it -p 8888:8888 -v "${PWD}"/metabopipeline_notebooks:/home/jovyan/work maxvin/data_science_img:0.1.5
+docker run -it -p 8888:8888 -v "${PWD}"/metabopipeline_notebooks:/home/jovyan/work ${img}
